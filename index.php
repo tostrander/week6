01398032336fd5a91e0d8843d96fba618d52b87e
tina <tostrander@greenriver.edu>
2021-05-04 14:16:48 -0400

    Added template tokens to home page view

--- index.php
+++ index.php
@@ -13,11 +13,16 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 //Define default route
-$f3->route('GET /', function(){
+$f3->route('GET /', function($f3){
+
+    //Add data to F3 hive
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
 
     //Display the home page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/info.html');
 });
 
 //Run Fat-Free
